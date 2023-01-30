c8aafd7f49de527e438fd085f0918e23d7b8a0ba
Michael Tobis <tobi4120@mylaurier.ca>
2023-01-30 00:24:24 -0500

    job tracker - load data

--- frontend/src/components/home/job_tracker/job_tracker.js
+++ frontend/src/components/home/job_tracker/job_tracker.js
@@ -1,8 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../../contexts';
+import { getJobsAppliedTo } from "../../../api/job_postings";
+import Loader from "../loader";
 
 export default function JobTracker(props) {
     const user = useContext(UserContext);
+    const [jobsAppledTo, setJobsAppliedTo] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        loadData();
+    }, [])
+
+    const loadData = async () => {
+        const response = await getJobsAppliedTo();
+        
+        if (response.status && response.status === 200) {
+
+            // Get only the jobs that the current user has applied to
+            const data = response.data.filter(x => x.id !== user.id)
+            console.log(data);
+        } else {
+            setError(true);
+            console.log(response)
+        }
+
+        setLoading(false);
+    }
+
+    if (loading) return <Loader />
+
+    if (error) return <div>Error: Data could not be loaded. Please refresh the page and try again.</div>
 
     return (
         <div>
