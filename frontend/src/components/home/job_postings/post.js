219bf0526228b787fe34873e87bab141f4a82ab8
Michael Tobis <tobi4120@mylaurier.ca>
2023-01-31 10:19:56 -0500

    display posts styling

--- frontend/src/components/home/job_postings/post.js
+++ frontend/src/components/home/job_postings/post.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PostPopUp from './post_pop_up';
-//import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function Post({ post }) {
     const addCommas = (num) => {
@@ -23,24 +23,33 @@ export default function Post({ post }) {
                 <img className='post-logo' src={post.employer_logo} />
                 <div className='post-title-name'>
                     <h3 className='post-job-title'>{post.job_title}</h3>
-                    <p>{post.employer_name}</p>
+                    <p className='post-company'>{post.employer_name}</p>
                 </div>
             </div>
 
             <p className='post-job-description'>{post.job_description}</p>
 
-            <div className='location'>
-                {/* <LocationOnIcon /> */}
-                <p>{post.job_city}, {post.job_state}, {post.job_country}</p>
-            </div>
+            <div className='post-footer'>
+                <div className='location'>
+                    <LocationOnIcon />
+                    <p>{post.job_city}, {post.job_state}, {post.job_country}</p>
+                </div>
+                
+                <div style={{ "flex": "1" }}>
+                    {post.job_min_salary && post.job_max_salary &&
+                        <div style={{ "display": "flex", "alignItems": "center" }}s>
+                            <span className='middle-dot' style={{ "color": "#5d6066" }}>&#183;</span>
+                            <p className='salary'>${addCommas(post.job_min_salary)} - ${addCommas(post.job_max_salary)}</p>
+                        </div>
+                    }
+                </div>
 
-            {post.job_min_salary && post.job_max_salary &&
-                <p className='salary'>${addCommas(post.job_min_salary)} - ${addCommas(post.job_max_salary)}</p>}
+                {/* Show popup */}
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#J${post.job_id.substring(0,post.job_id.length-2)}Modal`}>
+                    Show more info
+                </button>
+            </div>
             
-            {/* Show popup */}
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#J${post.job_id.substring(0,post.job_id.length-2)}Modal`}>
-                Show more info
-            </button>
             
             {/* Popup */}
             <div className="modal fade" id={`J${post.job_id.substring(0,post.job_id.length-2)}Modal`} tabIndex="-1" aria-labelledby={`J${post.job_id.substring(0,post.job_id.length-2)}ModalLabel`} aria-hidden="true">
