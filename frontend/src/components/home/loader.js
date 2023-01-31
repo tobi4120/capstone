fbe4439ef1ce4f13aa277c3fc5cd3a25e73d70c2
Michael Tobis <tobi4120@mylaurier.ca>
2023-01-31 18:32:25 -0500

    loader styling

--- frontend/src/components/home/loader.js
+++ frontend/src/components/home/loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loader() {
     return (
-        <div className="d-flex justify-content-center">
+        <div style={{ "alignItems": "center", "height": "100%"}} className="d-flex justify-content-center">
             <div className="spinner-border text-primary" role="status">
                 <span className="visually-hidden">Loading...</span>
             </div>
