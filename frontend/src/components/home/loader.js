c7f2590f77fa6eae67dbe74be3e79093a1ce7de1
Michael Tobis <tobi4120@mylaurier.ca>
2023-01-31 15:03:41 -0500

    loader styling

--- frontend/src/components/home/loader.js
+++ frontend/src/components/home/loader.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Loader() {
     return (
-        <div>Loading...</div>
+        <div className="d-flex justify-content-center">
+            <div className="spinner-border text-primary" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
     )
 }
